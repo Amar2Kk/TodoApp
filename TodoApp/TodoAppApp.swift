9b115fce661bdaa3c49f4ac8260d4da0f959c419
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-05-14 21:38:37 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Ammar Abbas on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
